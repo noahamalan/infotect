7dc7c6c8e00c4396e025d54f27972d67de675d41
Amalan Premkumar <amalanpremkumar@gmail.com>
2017-01-21 13:17:47 +0530

    Login added

--- v1/login.php
+++ v1/login.php
@@ -40,13 +40,13 @@
         <div class="form-group">
             <label class="control-label col-sm-2" for="email">UserName:</label>
             <div class="col-sm-10">
-                <input type="email" class="form-control" id="email" placeholder="Enter User Name">
+                <input type="email" name="username" class="form-control" id="email" placeholder="Enter User Name">
             </div>
         </div>
         <div class="form-group">
             <label class="control-label col-sm-2" for="pwd">Password:</label>
             <div class="col-sm-10">
-                <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                <input type="password" name="password" class="form-control" id="pwd" placeholder="Enter password">
             </div>
         </div>
         <div class="form-group">
@@ -58,7 +58,7 @@
         </div>
         <div class="form-group">
             <div class="col-sm-offset-2 col-sm-8">
-                <button type="submit" class="btn btn-success btn-block">Submit</button>
+                <input type="submit" name="submit" value="Submit"> 
                 <a href="ForgotPWD.php"<button class="btn btn-info btn-block">Forgot Password</button></a>
 
 
@@ -79,25 +79,15 @@
 <?php
 //Post Request sent from form
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $con = create_DB_connection();
-    echo $con;
-    // if($con){
-    //     $sql = prepare_add_query($name , $teamname);
-    //     $message = execute_query($con , $sql);
-    //     echo $message;
-    // } 
-
-
-
     //Accesses the name that was added in the form
-    // add_data($_POST["name"] , $_POST["teamname"]);
+    add_data($_POST["username"] , $_POST["password"]);
 }
 
 //Adding data to database
-function add_data($name , $teamname){
+function add_data($username , $password){
     $con = create_DB_connection();
     if($con){
-        $sql = prepare_add_query($name , $teamname);
+        $sql = prepare_add_query($username , $password);
         $message = execute_query($con , $sql);
         echo $message;
     }
@@ -110,19 +100,23 @@ function create_DB_connection(){
 }
 
 //Preparing the query to add data to database
-function prepare_add_query($name , $teamname){
-    $sql = "INSERT INTO user (username , password , usertype) VALUES ('".$name."','".$teamname."')";
+function prepare_add_query($username , $password){
+    $sql = "SELECT * from user where username = '".$username."' AND  password = '".$password."'";
     return $sql;
 }
 
 
 //Executing Query to add data to database
 function execute_query($connection , $sql){
-    if ($connection->query($sql) === TRUE) {
-        $message = "New record added successfully";
+    $result = $connection->query($sql);
+    if ($result->num_rows > 0) {
+        // output data of each row
+        // while($row = $result->fetch_assoc()) {
+        //     echo "username: " . $row["username"]. " - password: " . $row["password"]."<br>";
+        // }
+        header( "Location: /infotect/v1/home.php" );
     } else {
-        $message =  "Error: " . $sql . "<br>" . $connection->error;
+        echo "Invalid login";
     }
-    return $message;
 }
 ?>
